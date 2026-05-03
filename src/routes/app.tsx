b0c0583a6f363faf7559de26a5aba0519b758d46
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2026-05-03 11:52:02 +0700

    update form generate soal dan revamp export doc

--- src/routes/app.tsx
+++ src/routes/app.tsx
@@ -186,7 +186,7 @@ function AppLayout() {
         </header>
 
         <main className="flex-1 px-4 py-6 sm:px-6 sm:py-8 lg:px-8">
-          <div className="mx-auto max-w-6xl">
+          <div className="mx-auto max-w-8xl">
             <Outlet />
           </div>
         </main>
